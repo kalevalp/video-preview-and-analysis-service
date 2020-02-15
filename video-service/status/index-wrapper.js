6b04661f36099e5e2976a4b3a25f718030db1066
Kalev Alpernas <kalev.alp@gmail.com>
2020-02-16 01:27:40 +0200

    watchtower integration

---
+++ video-service/status/index-wrapper.js
@@ -0,0 +1,15 @@
+const recorder = require('watchtower-recorder');
+const eventsStreamName = process.env['WATCHTOWER_EVENT_KINESIS_STREAM'];
+const debug = process.env.DEBUG_WATCHTOWER;
+
+// Loading modules that fail when required via vm2
+const aws = require('aws-sdk');
+
+let context, lambdaExecutionContext, lambdaInputEvent;
+function updateContext(name, event, lambdaContext) { context = name; lambdaExecutionContext = lambdaContext; lambdaInputEvent = event; }
+
+const mock = {
+    'aws-sdk' : aws,
+};
+
+module.exports.handler = recorder.createRecordingHandler('status/index.js', 'handler', mock, false, updateContext, true);
