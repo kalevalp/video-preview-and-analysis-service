dfd625a6bb6434f8cd0b2605909dfeb516103933
Kalev Alpernas <kalev.alp@gmail.com>
2020-02-26 17:56:25 +0200

    Proper S3 event recording

--- upload-service/status/index-wrapper.js
+++ upload-service/status/index-wrapper.js
@@ -1,5 +1,8 @@
 const recorder = require('watchtower-recorder');
 const eventsStreamName = process.env['WATCHTOWER_EVENT_KINESIS_STREAM'];
+const publisher = recorder.createEventPublisher(eventsStreamName);
+const crypto = require('crypto');
+
 const debug = process.env.DEBUG_WATCHTOWER;
 
 // Loading modules that fail when required via vm2
@@ -9,8 +12,21 @@ const cp = require('child_process');
 let context, lambdaExecutionContext, lambdaInputEvent;
 function updateContext(name, event, lambdaContext) { context = name; lambdaExecutionContext = lambdaContext; lambdaInputEvent = event; }
 
+
+const proxyConditions = [];
+proxyConditions.push({
+    scope: 'S3',
+    functionName: 'getObject',
+    condition: () => true,
+    opInSucc: (argumentsList) => (response) => {
+        const hash = crypto.createHash('sha256');
+        hash.update(response.data.Body);
+        publisher({name: "GOT_OBJECT", params: {key: argumentsList[0].Key, data: hash.digest('base64')}}, lambdaExecutionContext, Date.now())
+    }
+})
+
 const mock = {
-    'aws-sdk' : aws,
+    'aws-sdk' : recorder.createAWSSDKMock(proxyConditions),
     'child_process': {
         'exec': cp.exec,
         'spawn': (...params) => {
