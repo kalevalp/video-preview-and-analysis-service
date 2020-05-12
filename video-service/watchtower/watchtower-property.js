ade0c5cdaa9fa7f9ac751124fb5e299301b0c9d0
Kalev Alpernas <kalev.alp@gmail.com>
2020-05-12 21:42:05 +0300

    updated

--- video-service/watchtower/watchtower-property.js
+++ video-service/watchtower/watchtower-property.js
@@ -4,20 +4,6 @@
  *   <description>
  *
  * ******************************************************************************** */
-const properties = [
-    {
-        name: '<name>',
-        quantifiedVariables: ['var1','var2',...],
-        projections: [['var1'],...,['var1','var2',...]],// All relevant subsets, always including the full set.
-        stateMachine: {
-            '<EVENT_ID>': {
-                params: [ 'var1', ... ],
-                '<STATE>' : { to: '<STATE>' }, // Special states include the required INITIAL and FAILURE, and the optional SUCCESS
-                ...
-            },
-            ...
-        }
-    },
-];
+const properties = [];
 
 module.exports = properties;
